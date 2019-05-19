260cde2b7a347e31c774439d87e9e2db49ac8883
chaithanya <chaithanyag.619@gmail.com>
2019-05-19 21:54:44 +0530

    Imported files of admin folder of views folder.
    Successfully imported files of admin folder of views folder.

---
+++ application/views/admin/admin_login_footer.php
@@ -0,0 +1,14 @@
+
+                <div>
+                <h1>Designer Junction</h1>
+                  <p>©2018 All Rights Reserved. <a href="http://www.nyesteventuretech.com">Nyeste Venture Technologies.</a></p>
+                </div>
+                 </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  </body>
+  
+</html>
